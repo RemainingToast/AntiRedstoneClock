23b0cbdc8720c57fe55d7bf6768aea02db6ca2a9
Amosar <maxi.cass11@gmail.com>
2021-10-14 13:26:41 +0200

    start working on plotSquared6

--- DEPENDENCIES/PlotSquared/PLOTSQUARED_6/src/main/java/com/trafalcraft/antiRedstoneClock/util/plotSquared/PlotSquared_6.java
+++ DEPENDENCIES/PlotSquared/PLOTSQUARED_6/src/main/java/com/trafalcraft/antiRedstoneClock/util/plotSquared/PlotSquared_6.java
@@ -1,6 +1,25 @@
 package com.trafalcraft.antiRedstoneClock.util.plotSquared;
 
-public class PlotSquared_6 {
+import com.trafalcraft.anti_redstone_clock.util.plotSquared.IPlotSquared;
 
-    
+import com.plotSquared.core.PlotSquared;
+
+import org.bukkit.Location;
+
+public class PlotSquared_6 implements IPlotSquared{
+
+    @Override
+    public void init() {
+        PlotSquared.toString();
+    }
+
+    @Override
+    public boolean isAllowedPlot(Location location) {
+        return false;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
 }
